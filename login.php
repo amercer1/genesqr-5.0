1fd3c201f8090be2471170b015ed84ac58f3eb28
Andre Mercer <amercer1@email.arizona.edu>
2013-01-25 21:42:55 -0700

    Added comments

--- login.php
+++ login.php
@@ -8,25 +8,44 @@
 <body>
 <h2>Genseqr-5.0</h2>
 <br />
+
+<!-- Create a form, action will send all data to process.php with a post method. In process.php you will find user entered
+info in $_POST object-->
 <form class="form-horizontal" action="process.php" method="post">
+ 
+  <!-- Style div  -->
   <div class="control-group">
+     <!-- label  -->
     <label class="control-label" name="login"for="login">iPlant Username</label>
     <div class="controls">
+	<!-- username inputbox -->
       <input type="text" name="login" id="login" placeholder="Username">
     </div>
   </div>
-  <div class="control-group">
+ 
+
+  <!--styel div  -->
+ <div class="control-group">
+     <!-- lable -->
     <label class="control-label" name="password" for="password">Password</label>
     <div class="controls">
+      <!--username password box -->
       <input type="password" name="password" id="inputPassword" placeholder="Password">
     </div>
   </div>
-  <div class="control-group">
+ 
+ <!-- style div -->
+ <div class="control-group">
     <div class="controls">
+       <!-- submit button -->
       <button type="submit" class="btn">Sign in</button>
     </div>
   </div>
+
 </form>
+
+<!-- Close form -->
+
 <?php
 
 session_start();
