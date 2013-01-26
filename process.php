2767051a33ebdf9dafa46b1359661c25c6be2873
Andre Mercer <amercer1@email.arizona.edu>
2013-01-25 23:00:58 -0700

    Added comments

--- process.php
+++ process.php
@@ -8,19 +8,24 @@
 <body>
 <?php
 
-//session_start();
+// pull $_POST values and place them in variables 
 $username=$_POST['login'];
 $password=$_POST['password'];
 
+// assign them to session cookies variables so that user can be logged in through a session
 $_SESSION['login']=$username;
 $_SESSION['password']=$password;
 
 // Authentication of a user
 
+//Create a php curl object    
 $ch = curl_init();
 
+
+//Base url for foundation api authentication for php curl  
 $auth_url = 'http://foundation.iplantc.org/auth-v1/';
 
+//Set php curl options. 
 curl_setopt($ch, CURLOPT_URL,$auth_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_POST, true);
@@ -28,9 +33,11 @@ $ch_values = "$username:$password";
 curl_setopt($ch, CURLOPT_USERPWD, $ch_values);
 // Getting results
 
+//Execute the php curl and grab the response 
 $response = curl_exec($ch);                                          
 $resultStatus = curl_getinfo($ch);                                   
 
+//If response went well, print three forms for user to use.  If not ask for user to re-enter data
 if($resultStatus['http_code'] == 200) {
      echo '<h2>Create a job</h2><br />';
            
@@ -145,18 +152,17 @@ if($resultStatus['http_code'] == 200) {
     </div>
   </div>
 </form>';
+
+// Take response and turn it into json
            $handled_json = json_decode($response,true);
+
+//grab the token and place it in a session variable
            $TOKEN=$handled_json['result']['token'];
            $_SESSION['token']=$TOKEN;
+
+
 } else if($resultStatus['http_code'] == 401){
     echo 'Login Failed ';
-/*    echo '<form action="process.php" method="post">
-          Login <input name="login" type="text" />
-          Password <input name="password" type="password" />
-
-          <input type="submit" />
-          </form> ';                         
-*/
 
     echo'<form class="form-horizontal" action="process.php" method="post">
   <div class="control-group">
@@ -181,6 +187,8 @@ if($resultStatus['http_code'] == 200) {
 
     echo 'Login Failed '.print_r($resultStatus);
 }
+
+//Destroy php curl object
 curl_close ($ch);
 
 ?>
